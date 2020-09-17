a45a169b2b05cb09c92daf863351b905a570b5c2
Bogdan <>
2020-09-17 07:14:20 +0300

    Код из методички

---
+++ lab1_mapping.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+class Mapping
+{
+	int M[33];
+public:
+	Mapping() 
+	{
+		Clear();
+	}
+	void Assign(char d, int Md) 
+	{
+		M[d - 'а'] = Md;
+	}
+	bool Compute(char d, int& r)
+	{
+		if (M[d - 'а'] >= 0)
+		{
+			r = M[d - 'а'];
+			return true;
+		}
+		else
+			return false;
+	}
+	void Clear()
+	{ 
+		for (int i = 0; i < 33; i++) M[i] = -1;
+	}
+};
+int main()
+{
+	return 0;
+}
